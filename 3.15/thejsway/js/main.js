f756d73066369b05b11103325c150a7963342a3c
notopoli <notopoli@gmail.com>
2022-06-07 22:39:12 -0500

    commit 2

---
+++ 3.15/thejsway/js/main.js
@@ -0,0 +1,32 @@
+//Presentation
+console.log('Linh')
+console.log(24)
+
+//Minimalistic calculator
+console.log(6+3)
+console.log(6-3)
+console.log(6*3)
+console.log(6/3)
+
+//Values Prediction
+9;
+4 + 5;
+45;
+
+//Improved hello
+const name = prompt('Enter your first and last name: ');
+alert(`Hello, ${name}`);
+
+//Final values: 2, 10, (120), 30, 40, 10, 10 (c !== 120, c == 102)
+let a = 2;
+a -= 1;
+a++;
+let b = 8;
+b += 2;
+const c = a + b * b;
+const d = a * b + b;
+const e = a * (b + b);
+const f = a * b / a;
+const g = b / a * a;
+console.log(a, b, c, d, e, f, g)
+
---
+++ Zellwk-simple-crud-app/server.js
@@ -0,0 +1,12 @@
+// Express
+
+const express = require ('express');
+const app = express();
+
+app.listen(3000, function (){
+    console.log('listen on 3000')
+})
+
+// CRUD - READ
+
+console.log('this is a statement')
